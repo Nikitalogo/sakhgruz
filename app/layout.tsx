fc83a84cc8784534ce6ca438e9a10cb744da3420
Nikitalogo <lopapedos@gmail.com>
2026-03-11 22:13:37 +1100

    feat: yandex metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Bebas_Neue, Montserrat } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const bebas = Bebas_Neue({
@@ -38,6 +39,11 @@ export default function RootLayout({
       <body className={`${bebas.variable} ${montserrat.variable} antialiased bg-[#111827] text-white`}>
         {children}
       </body>
+      <Script id="yandex-metrika" strategy="afterInteractive">{`
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=107254714','ym');
+  ym(107254714,'init',{webvisor:true,clickmap:true,accurateTrackBounce:true,trackLinks:true});
+`}</Script>
+      <noscript><img src="https://mc.yandex.ru/watch/107254714" style={{position:'absolute',left:'-9999px'}} alt="" /></noscript>
     </html>
   )
 }
